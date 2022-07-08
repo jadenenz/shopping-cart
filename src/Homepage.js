7fe4c8b68a3cff2ef83611cc6ad8ab9d3fe8ec39
Jaden <jenzbr@gmail.com>
2022-07-08 15:30:51 -0500

    first commit

---
+++ src/Homepage.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Homepage() {
+    return (
+        <div className="homepage--container">
+            <h1>Magic Angels Home Page</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed venenatis viverra nulla, quis congue enim tempor non. Suspendisse potenti. Ut suscipit quam at porttitor egestas. Praesent vulputate nisi condimentum massa efficitur gravida. Mauris luctus hendrerit ligula, eget gravida justo scelerisque ut. Donec laoreet nibh sit amet volutpat laoreet. Proin tristique iaculis gravida. Quisque sit amet posuere est. Donec pretium, sem quis bibendum blandit, massa ex commodo neque, eu sagittis ante eros sit amet ipsum. Sed rutrum vestibulum nulla et scelerisque. Praesent non ex ut dolor lobortis varius vitae eget mauris. </p>
+        </div>
+    )
+}
+
+export default Homepage
---
+++ src/Shop.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Shop() {
+    return (
+        <div className="shop--container">
+            <h1>Magic Angels Shop</h1>
+            <div>Product cards go here</div>
+        </div>
+    )
+}
+
+export default Shop
